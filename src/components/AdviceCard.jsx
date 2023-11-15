334cf4405f0da453be05a2511a70c233d8015f79
E-sims <livecultr@gmail.com>
2023-11-15 09:39:41 -0600

    Set button hover glow and og image

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -24,20 +24,17 @@ const AdviceCard = () => {
     return (
       <div className="w-[343px] h-auto md:w-[540px] rounded-xl p-6 md:p-12 bg-drkGray flex flex-col items-center justify-center relative shadow-darkBlue shadow-sm">
         <p id="quote-id" className="text-neon mb-6 font-medium">
-          &quot;Advice&quot;
+          Advice
         </p>
         <p className="text-cyan quote-text text-center font-extrabold max-w-[444px]">
-          &quot;Loading...Guru is thinking...&quot;
+          &quot;Loading...Guru is thinking&quot;
         </p>
         <div className="flex items-center justify-center mt-5 mb-10 gap-x-4">
           <span className="w-[122px] md:w-[196px] h-[1px] bg-cyan rounded-full z-10"></span>
           <img src="/pattern-divider-mobile.svg" alt="divider" />
           <span className="w-[122px] md:w-[196px] h-[1px] bg-cyan rounded-full z-10"></span>
         </div>
-        <div
-          className="absolute -bottom-8 rounded-full bg-neon flex items-center justify-center w-16 h-16 cursor-pointer"
-          onClick={getQuote}
-        >
+        <div className="absolute transition-all -bottom-8 rounded-full bg-neon flex items-center justify-center w-16 h-16 cursor-pointer hover:blur-lg">
           <img src="/icon-dice.svg" alt="dice icon" />
         </div>
       </div>
@@ -45,7 +42,7 @@ const AdviceCard = () => {
   }
 
   return (
-    <div className="w-[343px] h-auto md:w-[540px] rounded-xl p-6 md:p-12 bg-drkGray flex flex-col items-center justify-center relative shadow-darkBlue shadow-sm">
+    <div className="w-[343px] h-auto md:w-[540px] rounded-xl p-6 md:p-12 bg-drkGray flex flex-col items-center justify-center relative shadow-darkBlue shadow-sm z-0">
       <p id="quote-id" className="text-neon mb-6 font-medium">
         {id}
       </p>
@@ -58,7 +55,7 @@ const AdviceCard = () => {
         <span className="w-[122px] md:w-[196px] h-[1px] bg-cyan rounded-full z-10"></span>
       </div>
       <div
-        className="absolute -bottom-8 rounded-full bg-neon flex items-center justify-center w-16 h-16 cursor-pointer"
+        className="absolute -bottom-8 rounded-full bg-neon flex items-center justify-center w-16 h-16 cursor-pointer z-10 hover:button-shadow transition-shadow ease-in-out"
         onClick={getQuote}
       >
         <img src="/icon-dice.svg" alt="dice icon" />
